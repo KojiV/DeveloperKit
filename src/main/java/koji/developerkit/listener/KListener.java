6b3ef553f661cc6c15e5580ddd881e55af3b52f7
G-K-G <69867605+G-K-G@users.noreply.github.com>
2022-10-28 15:22:58 -0400

    feat: removal of annotation (just going to add tutorial in repo)

--- src/main/java/koji/developerkit/listener/KListener.java
+++ src/main/java/koji/developerkit/listener/KListener.java
@@ -1,44 +1,7 @@
 package koji.developerkit.listener;
 
 import koji.developerkit.KBase;
-import koji.developerkit.events.GenericEvent;
-import koji.developerkit.events.GenericEventHandler;
-import org.bukkit.Bukkit;
-import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
 
-import java.lang.IllegalAccessException;
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
-import java.util.function.Consumer;
-
 public class KListener extends KBase implements Listener {
-    public KListener() {
-        Class<?> clazz = getClass();
-        for(Method method : clazz.getMethods()) {
-            GenericEventHandler annotation = method.getDeclaredAnnotation(GenericEventHandler.class);
-
-            if(annotation != null && method.getParameterTypes().length > 0) {
-                Class<? extends Event>[] exclusions = annotation.excludedEvents();
-                Class<? extends Event> eventType = (Class<? extends Event>) method.getParameterTypes()[0];
-
-                Bukkit.getPluginManager().registerEvents(GenericEvent.newInstance(
-                    eventType,
-                    toConsumer(this, method),
-                    exclusions
-                ), KBase.getPlugin());
-                
-            }
-        }
-    }
-
-    public static <T> Consumer<T> toConsumer(Object obj, Method m) {
-        return param -> {
-            try {
-                m.invoke(obj, param);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
 }
