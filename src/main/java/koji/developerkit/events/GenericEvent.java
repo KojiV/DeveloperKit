526fcd52826e989674dbe33a9fb0e26cae2727da
Koji <69867605+KojiV@users.noreply.github.com>
2022-10-28 12:14:37 -0400

    style: testing

--- src/main/java/koji/developerkit/events/GenericEvent.java
+++ src/main/java/koji/developerkit/events/GenericEvent.java
@@ -1,35 +1,33 @@
 package koji.developerkit.events;
 
 import koji.developerkit.events.ExpandEventHandler;
-import koji.developerkit.events.GenericEventHandler;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 
-import java.lang.annotation.Annotation;
 import java.util.function.Consumer;
 
 public class GenericEvent<T extends Event> extends Event implements Listener {
 
     private final Consumer<? super T> mainThing;
-    private final GenericEventHandler annotation;
+    private final Class<? extends Event>[] exclusions;
 
     public static <A extends Event> GenericEvent<A> newInstance(Class<A> type, Consumer<? super A> mainThing) {
         return new GenericEvent<A>(mainThing);
     }
 
-    public static <A extends Event> GenericEvent<A> newInstance(Class<A> type, Consumer<? super A> mainThing, GenericEventHandler annotation) {
+    public static <A extends Event> GenericEvent<A> newInstance(Class<A> type, Consumer<? super A> mainThing, Class<? extends Event>... exclusions) {
         return new GenericEvent<A>(mainThing, exclusions);
     }
 
     public GenericEvent(Consumer<? super T> mainThing) {
         this.mainThing = mainThing;
-        this.annotation = new GenericEventHandler();
+        this.exclusions = (Class<? extends Event>[]) new Class[0];
     }
 
-    public GenericEvent(Consumer<? super T> mainThing, GenericEventHandler annotation) {
+    public GenericEvent(Consumer<? super T> mainThing, Class<? extends Event>... exclusions) {
         this.mainThing = mainThing;
-        this.annotation = annotation;
+        this.exclusions = exclusions;
     }
 
     private static final HandlerList handlers = new HandlerList();
@@ -42,7 +40,7 @@ public class GenericEvent<T extends Event> extends Event implements Listener {
         return handlers;
     }
 
-    @ExpandEventHandler(exclude = annotation.exclude())
+    @ExpandEventHandler(exclude = exclusions)
     public void event(T generic) {
         mainThing.accept(generic);
     }
