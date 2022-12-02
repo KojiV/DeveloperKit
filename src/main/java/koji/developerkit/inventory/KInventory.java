ec07fbd8da704493c30637d48f05238be7184b4b
KojiV <glennvanvalkenburghgk@gmail.com>
2022-12-02 14:32:02 -0500

    feat: trying stuff with kinventory

---
+++ src/main/java/koji/developerkit/inventory/KInventory.java
@@ -0,0 +1,105 @@
+package koji.developerkit.inventory;
+
+import koji.developerkit.KBase;
+import koji.developerkit.gui.GUIClickableItem;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class KInventory extends KBase {
+    private final int size;
+    private final Inventory inv;
+    private final String title;
+
+    public KInventory(String title, int size) {
+        this.size = size;
+        this.title = title;
+
+        inv = Bukkit.createInventory(null, size, title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Inventory getInventory() {
+        return inv;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public ItemStack[] getConstantSlots() {
+        ItemStack[] constants = new ItemStack[size];
+        for(int i = 0; i < inv.getSize(); i++) {
+            if(inv.getItem(i) != null) {
+                constants[i] = inv.getItem(i);
+            }
+        }
+        return constants;
+    }
+
+    public abstract void setConstants();
+
+    public KInventory set(int slot, ItemStack item) {
+        inv.setItem(slot, item);
+        return this;
+    }
+
+    public KInventory set(GUIClickableItem item) {
+        set(inv, item);
+        return this;
+    }
+
+    public static class PlayerInstance extends KBase {
+        private final Player player;
+        private final KInventory base;
+        private final Inventory inventory;
+        private String title;
+
+        public PlayerInstance(Player player, KInventory base) {
+            this.player = player;
+            this.base = base;
+            this.inventory = base.getInventory();
+            this.title = base.getTitle();
+        }
+
+        public Player getPlayer() {
+            return player;
+        }
+
+        public Inventory getInventory() {
+            return inventory;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public PlayerInstance setTitle(String title) {
+            if(player.getOpenInventory().getTopInventory().equals(inventory)) {
+                InventoryUpdate.updateInventory(player, title);
+                this.title = title;
+            }
+            return this;
+        }
+
+        public PlayerInstance reset(String title) {
+            setTitle(title);
+            inventory.setContents(base.getConstantSlots());
+            return this;
+        }
+
+        public PlayerInstance set(int slot, ItemStack item) {
+            inventory.setItem(slot, item);
+            return this;
+        }
+
+        public PlayerInstance set(GUIClickableItem item) {
+            set(inventory, item);
+            return this;
+        }
+    }
+}
