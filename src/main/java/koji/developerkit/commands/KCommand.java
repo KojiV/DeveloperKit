0c037f383b3aeefb3e370da6bc63c08dcfad9654
G-K-G <69867605+G-K-G@users.noreply.github.com>
2022-12-03 11:44:31 -0500

    feat: stuff for registering commands in funny cool way

--- src/main/java/koji/developerkit/commands/KCommand.java
+++ src/main/java/koji/developerkit/commands/KCommand.java
@@ -3,35 +3,60 @@ package koji.developerkit.commands;
 import koji.developerkit.KBase;
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.logging.Level;
 
 public abstract class KCommand extends KBase implements CommandExecutor {
 
+    public KCommand() {
+
+    }
+
     @Override
     public abstract boolean onCommand(CommandSender sender, Command command, String label, String[] args);
 
-    @SuppressWarnings("unchecked")
+    public CommandMap getCommandMap() {
+        try {
+            return (CommandMap) getPrivateField(Bukkit.getPluginManager(), "commandMap");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public void registerCommand(String cmd) {
         try {
-            for(Command command : PluginCommandYamlParser.parse(getPlugin())) {
-                if(command.getName().equalsIgnoreCase(cmd)) {
-                    CommandMap map = (CommandMap) getPrivateField(Bukkit.getPluginManager(),
-                            "commandMap");
-                    HashMap<String, Command> knownCommands = (HashMap<String, Command>)
-                            getPrivateField(map, "knownCommands");
-                    knownCommands.put(command.getName(), command);
-                    for(String alias : command.getAliases()) {
-                        knownCommands.put(alias, command);
-                    }
-                    ((PluginCommand) command).setExecutor(this);
+            for (Command command : PluginCommandYamlParser.parse(getPlugin())) {
+                if (command.getName().equals(cmd)) {
+                    Command pluginCommand = new Command(
+                            command.getName(),
+                            command.getDescription(),
+                            command.getUsage(),
+                            command.getAliases()
+                    ) {
+                        @Override
+                        public boolean execute(
+                                @NotNull CommandSender sender,
+                                @NotNull String commandLabel,
+                                @NotNull String[] args
+                        ) {
+                            if (testPermission(sender)) {
+                                return onCommand(sender, this, commandLabel, args);
+                            } else {
+                                return false;
+                            }
+                        }
+                    };
+                    CommandMap commandMap = getCommandMap();
+                    commandMap.register(KBase.getPlugin().getName(),  pluginCommand);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             getPlugin().getLogger().log(
                     Level.WARNING,
-                    "§4The command §c"+cmd+" §4couldn't be found in §cplugin.yml§4l!"
+                    "§4The command §c" + cmd + " §4couldn't be found in §cplugin.yml§4l!"
             );
         }
     }
@@ -39,22 +64,19 @@ public abstract class KCommand extends KBase implements CommandExecutor {
     @SuppressWarnings("unchecked")
     public void unregisterCommand(String cmd) {
         try {
-            for(Command command : PluginCommandYamlParser.parse(getPlugin())) {
-                if(command.getName().equalsIgnoreCase(cmd)) {
-                    CommandMap map = (CommandMap) getPrivateField(Bukkit.getPluginManager(),
-                            "commandMap");
-                    HashMap<String, Command> knownCommands = (HashMap<String, Command>)
-                            getPrivateField(map, "knownCommands");
-                    knownCommands.remove(command.getName(), command);
-                    for(String alias : command.getAliases()) {
-                        knownCommands.remove(alias, command);
-                    }
+            for (Command command : PluginCommandYamlParser.parse(getPlugin())) {
+                if (command.getName().equals(cmd)) {
+                    CommandMap commandMap = getCommandMap();
+                    HashMap<String, Command> knownCommands =
+                            (HashMap<String, Command>) getPrivateField(commandMap, "knownCommands");
+                    knownCommands.remove(cmd);
+                    command.unregister(commandMap);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             getPlugin().getLogger().log(
                     Level.WARNING,
-                    "§4The command §c"+cmd+" §4couldn't be found in §cplugin.yml§4l!"
+                    "§4The command §c" + cmd + " §4couldn't be found in §cplugin.yml§4l!"
             );
         }
     }
