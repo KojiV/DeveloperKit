8a7c6597b3e558a56d0e2e7658ba91edfa8ca29a
KojiV <glennvanvalkenburghgk@gmail.com>
2023-05-19 14:34:06 -0400

    feat: gave safemap more utility

--- src/main/java/koji/developerkit/utils/SafeMap.java
+++ src/main/java/koji/developerkit/utils/SafeMap.java
@@ -17,4 +17,13 @@ public class SafeMap <A, B> extends HashMap<A, B> {
     public B get(Object key, B defaultValue) {
         return super.getOrDefault(key, defaultValue);
     }
+
+    public B createIfNotPresent(A key) {
+        return createIfNotPresent(key, defaultValue);
+    }
+
+    public B createIfNotPresent(A key, B defaultValue) {
+        if(!containsKey(key)) put(key, defaultValue);
+        return get(key, defaultValue);
+    }
 }
