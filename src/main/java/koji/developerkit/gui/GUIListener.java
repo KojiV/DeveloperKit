ff46b04faa9fec7419462b9e960758ebf6ae64c7
G-K-G <69867605+G-K-G@users.noreply.github.com>
2023-05-20 19:19:21 -0400

    feat: made accidental double clicks not be an issue

--- src/main/java/koji/developerkit/gui/GUIListener.java
+++ src/main/java/koji/developerkit/gui/GUIListener.java
@@ -6,8 +6,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
 
 public class GUIListener extends KListener {
+
+    public GUIListener(){
+        timeSinceLastClick = new HashMap<>();
+    }
 
     /**
      * Registers the listener so it actually works
@@ -18,6 +25,9 @@ public class GUIListener extends KListener {
         );
     }
 
+    private final HashMap<Inventory, HashMap<Integer, Long>> timeSinceLastClick;
+    private final static long COOLDOWN = 100;
+
     /**
      * Checks if there is a GUIClickable item to grab, if yes run its run function
      *
@@ -27,19 +37,28 @@ public class GUIListener extends KListener {
     public void onInventoryClick(InventoryClickEvent e) {
         if(e.getSlot() < e.getInventory().getSize()) {
             if (isValidItem(e.getCurrentItem())) {
+                Inventory inv = e.getClickedInventory();
+                if(inv != null && !timeSinceLastClick.containsKey(inv))
+                    timeSinceLastClick.put(inv, new HashMap<>());
                 try { NBTItem item = new NBTItem(e.getCurrentItem());
                     if (item.hasKey("ClickItem") && item.getString("ClickItem") != null) {
-                        GUIClickableItem guiItem =
-                                GUIClickableItem.getItemsToRun().get(
-                                        item.getString("ClickItem")
-                                );
-                        if (guiItem == null) return;
-                        try {
-                            guiItem.run(e);
-                        } catch (Exception exception) {
-                            exception.printStackTrace();
+                        GUIClickableItem guiItem = GUIClickableItem.getItemsToRun().get(
+                                item.getString("ClickItem")
+                        );
+                        if(guiItem != null) {
+                            if(!timeSinceLastClick.get(inv).containsKey(e.getRawSlot()) ||
+                                    timeSinceLastClick.get(inv).get(e.getRawSlot()) +
+                                            COOLDOWN < System.currentTimeMillis()
+                            ) {
+                                try {
+                                    guiItem.run(e);
+                                } catch (Exception exception) {
+                                    exception.printStackTrace();
+                                }
+                                timeSinceLastClick.get(inv).put(e.getRawSlot(), System.currentTimeMillis());
+                            }
+                            e.setCancelled(e.isCancelled() || !guiItem.canPickup());
                         }
-                        e.setCancelled(e.isCancelled() || !guiItem.canPickup());
                     }
                 } catch (NullPointerException ignored) {}
             }
